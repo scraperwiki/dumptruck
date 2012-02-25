5a103b92d06a1590f1aa7227320eef347196e8c8
Thomas Levine <thomas.levine@gmail.com>
2012-02-25 06:58:33 -0500

    aoeu

--- highwall.py
+++ highwall.py
@@ -11,12 +11,6 @@ SQLITE_PYTHON_TYPE_MAP={
   u"real": float,
 }
 
-# Only for compatibility with scraperwiki;
-# we should use the SQLite names
-SWVARIABLES_PYTHON_TYPE_MAP={
-  u"float": float
-}
-
 PYTHON_SQLITE_TYPE_MAP={
   unicode: u"text",
   str: u"text",
@@ -24,85 +18,11 @@ PYTHON_SQLITE_TYPE_MAP={
   float: u"real",
 }
 
-class IndexList:
-  'Like running `PRAGMA index_list(?);`'
-  def __init__(self,highwall_object):
-    self.h = highwall_object
-
-  def __getitem__(self, table_name):
-    "Retrive the indices for a table."
-    return IndexInfo(self.h, table_name)
-
-  def __setitem__(self, index_info):
-    "I don't think this will ever be implemented."
-    print "This should not run."
-
-class IndexInfo:
-  'Like running `PRAGMA index_info(?);`'
-
-  class IndexNameError(Exception):
-    pass
-
-  def __init__(self,highwall_object, table_name):
-    self.h = highwall_object
-    self.table_name = table_name
-    self.table_indices = self.h.execute("PRAGMA index_list(`%s`)" % table_name, commit = False)
-
-  def __getitem__(self, index_name):
-    "Retrive an index."
-    index = self.h.execute("PRAGMA index_info(`%s`)" % index_name, commit = False)
-
-    # Determine whether it's unique.
-    for i in self.table_indices:
-      if i['name'] == index_name:
-        unique = i['unique']
-        break
-
-    try:
-      unique
-    except NameError:
-      raise self.IndexNameError('There is no index with the name "%s".' % index_name)
-
-    return Index([col['name'] for col in index], unique = unique)
-
-  def __setitem__(self, index_name, index):
-    # This is currently a bit vulnerable to injection.
-    sql = "CREATE %s INDEX IF NOT EXISTS `%s` ON `%s` (%s)"
-    indexed_columns = ','.join(index.columns)
-    str_unique = 'UNIQUE' if index.unique else ''
-    self.h.execute(sql % (str_unique, index_name, self.table_name, indexed_columns))
-
-class Index:
-  "An index absent of a table"
-
-  #COLNAME_TYPES = set([unicode, str, int, IndexedColumn])
-  COLNAME_TYPES = set([unicode, str, int])
-
-  class DuplicateColumnError(Exception):
-    pass
-
-  def __init__(self, columns, unique = False):
-    self.unique = unique
-
-    # Set columns
-    if type(columns) in self.COLNAME_TYPES:
-      # One column
-      self.columns = [str(columns)]
-    elif set(map(type, columns)).issubset(self.COLNAME_TYPES):
-      # Multiple columns
-      str_columns = map(str, columns)
-      if len(set(str_columns)) != len(columns):
-        raise self.DuplicateColumnError
-      else:
-        self.columns = str_columns
-
-class IndexedColumn:
-  "This is how you do COLLATE or ASC."
-
-  def __init__(self, collate = None, sort = None):
-    self.collate = collate
-    self.sort = sort
-    raise NotImplementedError("IndexedColumn doesn't work yet.")
+# Only for compatibility with scraperwiki;
+# we should use the SQLite names
+SWVARIABLES_PYTHON_TYPE_MAP={
+  u"float": float
+}
 
 class MineCollapse(Exception):
   pass
@@ -142,9 +62,6 @@ class Highwall:
       raise TypeError("auto_commit must be a string")
     else:
       self.__vars_table = vars_table
-
-    # The index list
-    self.index_list = IndexList(self)
 
   def __check_or_create_table(self, table_name):
     if self.__is_table(table_name):
---
+++ removed.py
@@ -0,0 +1,80 @@
+
+class IndexList:
+  'Like running `PRAGMA index_list(?);`'
+  def __init__(self,highwall_object):
+    self.h = highwall_object
+
+  def __getitem__(self, table_name):
+    "Retrive the indices for a table."
+    return IndexInfo(self.h, table_name)
+
+  def __setitem__(self, index_info):
+    "I don't think this will ever be implemented."
+    print "This should not run."
+
+class IndexInfo:
+  'Like running `PRAGMA index_info(?);`'
+
+  class IndexNameError(Exception):
+    pass
+
+  def __init__(self,highwall_object, table_name):
+    self.h = highwall_object
+    self.table_name = table_name
+    self.table_indices = self.h.execute("PRAGMA index_list(`%s`)" % table_name, commit = False)
+
+  def __getitem__(self, index_name):
+    "Retrive an index."
+    index = self.h.execute("PRAGMA index_info(`%s`)" % index_name, commit = False)
+
+    # Determine whether it's unique.
+    for i in self.table_indices:
+      if i['name'] == index_name:
+        unique = i['unique']
+        break
+
+    try:
+      unique
+    except NameError:
+      raise self.IndexNameError('There is no index with the name "%s".' % index_name)
+
+    return Index([col['name'] for col in index], unique = unique)
+
+  def __setitem__(self, index_name, index):
+    # This is currently a bit vulnerable to injection.
+    sql = "CREATE %s INDEX IF NOT EXISTS `%s` ON `%s` (%s)"
+    indexed_columns = ','.join(index.columns)
+    str_unique = 'UNIQUE' if index.unique else ''
+    self.h.execute(sql % (str_unique, index_name, self.table_name, indexed_columns))
+
+class Index:
+  "An index absent of a table"
+
+  #COLNAME_TYPES = set([unicode, str, int, IndexedColumn])
+  COLNAME_TYPES = set([unicode, str, int])
+
+  class DuplicateColumnError(Exception):
+    pass
+
+  def __init__(self, columns, unique = False):
+    self.unique = unique
+
+    # Set columns
+    if type(columns) in self.COLNAME_TYPES:
+      # One column
+      self.columns = [str(columns)]
+    elif set(map(type, columns)).issubset(self.COLNAME_TYPES):
+      # Multiple columns
+      str_columns = map(str, columns)
+      if len(set(str_columns)) != len(columns):
+        raise self.DuplicateColumnError
+      else:
+        self.columns = str_columns
+
+class IndexedColumn:
+  "This is how you do COLLATE or ASC."
+
+  def __init__(self, collate = None, sort = None):
+    self.collate = collate
+    self.sort = sort
+    raise NotImplementedError("IndexedColumn doesn't work yet.")
