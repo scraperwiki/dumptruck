c446fb1ab0b2fbd2477db9e57d38ace82ad16bea
Thomas Levine <thomas.levine@gmail.com>
2012-04-18 18:46:42 -0400

    setup.py stuff

--- dumptruck/__init__.py
+++ dumptruck/__init__.py
@@ -1,1 +1,8 @@
 from dumptruck import *
+
+__title__ = 'dumptruck'
+__version__ = '0.1'
+#__build__ = 0x001101
+__author__ = 'Thomas Levine'
+__license__ = 'AGPL'
+__copyright__ = 'Copyright 2012 Thomas Levine'
--- setup.py
+++ setup.py
@@ -3,12 +3,28 @@
 from distutils.core import setup
 
 setup(name='DumpTruck',
-      version='0.1',
-      description='Relaxing interface to SQLite',
       author='Thomas Levine',
       author_email='thomas@scraperwiki.com',
+      url='https://github.com/tlevine/dumptruck',
+      description='Relaxing interface to SQLite',
       url='http://hacks.thomaslevine.com/dumptruck',
-      packages=['src'],
-     )
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: SQL',
+          'Topic :: Database',
+          'Topic :: Database :: Front-Ends',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft',
+          'Operating System :: POSIX',
+      ],
+      packages=['dumptruck'],
 
-#['sqlite3', 're', 'json', 'datetime', 'pickle', 'copy'],
+      # From requests
+    version=requests.__version__,
+    long_description=open('README.md').read() + '\n\n' + open('HISTORY.md').read(),
+      package_data={'': ['LICENSE']},
+      license=open('LICENSE').read(),
+     )
