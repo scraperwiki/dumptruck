bddf076ea72fedcee6af4e7d8f06f4d5769d42ba
Thomas Levine <occurrence@thomaslevine.com>
2012-08-01 10:57:28 -0400

    AGPL in i-nit

--- dumptruck/__init__.py
+++ dumptruck/__init__.py
@@ -24,5 +24,5 @@ __title__ = 'dumptruck'
 __version__ = '0.0.3'
 #__build__ = 0x001101
 __author__ = 'Thomas Levine'
-__license__ = 'AGPL'
+__license__ = 'GPL'
 __copyright__ = 'Copyright 2012 Thomas Levine'
