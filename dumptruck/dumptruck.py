e269e81c6528f4cb2744986fe92d56d2c463da0c
Thomas Levine <occurrence@thomaslevine.com>
2012-08-01 10:30:51 -0400

    switch from AGPL to GPL

--- dumptruck/dumptruck.py
+++ dumptruck/dumptruck.py
@@ -7,16 +7,16 @@
 # This file is part of DumpTruck.
 
 # DumpTruck is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero Public License as published by
+# it under the terms of the GNU Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
 # DumpTruck is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero Public License for more details.
+# GNU Public License for more details.
 
-# You should have received a copy of the GNU Affero Public License
+# You should have received a copy of the GNU Public License
 # along with DumpTruck.  If not, see <http://www.gnu.org/licenses/>.
 
 import sqlite3
