b8c4741b0374ef614922406b7557f6a99535701d
Thomas Levine <occurrence@thomaslevine.com>
2012-08-02 08:25:28 -0400

    adjust the update script

--- update.sh
+++ update.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 # Send a new version to the various places
 
-echo Checking out the master branch
-git checkout master
+if [ "$1" = "pypi" ]
+  then
 
-echo Uploading to PyPI
-python setup.py register sdist upload
+  echo Checking out the master branch
+  git checkout master
 
-echo Build the website
-cd website/build
-ant
-cd -
+  echo Uploading to PyPI
+  python setup.py register sdist upload
 
-echo Uploading the website
-scp -r website/publish/* www-data@thomaslevine.com:/srv/www/hacks/dumptruck
+elif [ "$1" = "website" ]
+  then
+
+  echo Build the website
+  (
+    cd website/build
+    ant
+  )
+
+  echo Uploading the website
+  scp -r website/publish/* www-data@thomaslevine.com:/srv/www/hacks/dumptruck
+
+else
+  echo "usage: $0 [command]"
+  echo \ 
+  echo Commands
+  echo "  $0 pypi      Upload the master branch to pypi."
+  echo "  $0 website   Build the website and upload it to the web server."
+fi
