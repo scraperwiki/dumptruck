d0ced5819951dbb21e13d0d89699c93b9b0c966a
Sean Duffy <seanduffy@live.com>
2014-08-27 11:21:05 +0100

    Add create_table method

--- dumptruck/dumptruck.py
+++ dumptruck/dumptruck.py
@@ -25,34 +25,94 @@
 
 import re
 import datetime
+import json
 from collections import OrderedDict
 import sqlalchemy
+from sqlalchemy.dialects.sqlite import TEXT, INTEGER, BOOLEAN, FLOAT, DATE, DATETIME 
 from convert import convert, quote, simplify
-from adapters_and_converters import register_adapters_and_converters, Pickle, replace_date_converter
 
 import old_dumptruck
+
+class JSONObject(sqlalchemy.TypeDecorator):
+    """Represents a dict, list or set as a json-encoded string."""
+
+    impl = sqlalchemy.String
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if type(value) == dict or type(value) == list or type(value) == set:
+                if type(value) == set:
+                    value = list(value)
+                value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+PYTHON_SQLITE_TYPE_MAP = {
+    unicode: TEXT,
+    str: TEXT,
+
+    int: INTEGER,
+    long: INTEGER,
+    bool: BOOLEAN,
+    float: FLOAT,
+
+    datetime.date: DATE,
+    datetime.datetime: DATETIME,
+
+    dict: JSONObject,
+    list: JSONObject,
+    set: JSONObject
+}
 
 class DumpTruck(old_dumptruck.DumpTruck):
     def __init__(self, dbname = 'dumptruck.db', vars_table = '_dumptruckvars', vars_table_tmp = '_dumptruckvarstmp', auto_commit = True, adapt_and_convert = True, timeout = 5):
         self.auto_commit = auto_commit
 
-        engine = sqlalchemy.create_engine('sqlite:///{}'.format(dbname), echo=True, connect_args={'timeout': timeout})
-        self.conn = engine.connect()
+        self.engine = sqlalchemy.create_engine('sqlite:///{}'.format(dbname), echo=True, connect_args={'timeout': timeout})
+        self.conn = self.engine.connect()
         self.trans = self.conn.begin()
 
     def execute(self, sql_query, *args, **kwargs):
         s = sqlalchemy.sql.text(sql_query)
-        result = self.conn.execute(s)
 
-        self.__commit_if_necessary(kwargs)
+        if kwargs.get('commit', self.auto_commit):
+            with self.conn.begin() as transaction:
+                result = self.conn.execute(s)
+        else:
+            result = self.conn.execute(s)
 
         if not result.returns_rows:
             return None
 
         return [OrderedDict(row) for row in result.fetchall()]
 
+    def insert(self, data, table_name='dumptruck', upsert=False, **kwargs):
+        pass
+
+    def create_table(self, data, table_name, **kwargs):
+        converted_data = convert(data)
+
+        if len(converted_data) == 0 or converted_data[0] == []:
+            raise ValueError('You passed no sample values, or all the values you passed were null.')
+        else:
+            startdata = OrderedDict(converted_data[0])
+
+        metadata = sqlalchemy.MetaData()
+        table = sqlalchemy.Table(table_name, metadata)
+
+        for column_name, column_value in startdata.items():
+            table.append_column(sqlalchemy.Column(column_name, self.get_column_type(column_value)))
+
+        with self.conn.begin() as transaction:
+            metadata.create_all(self.engine)
+
+    def get_column_type(self, column_value):
+        return PYTHON_SQLITE_TYPE_MAP[type(column_value)]
 
     def commit(self):
-        'Commit database transactions and start a new transaction.'
         self.trans.commit()
         self.trans = self.conn.begin()
